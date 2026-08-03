59f056338e7fa5e5ed4f9234cf46659002006c53
Adarsh20021 <adarshkulkarni10@gmail.com>
2026-08-03 14:48:46 +0530

    Restructuring listings

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const { error } = require("console");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
+const listings = require("./routes/listing.js");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main().then(()=>{
@@ -35,16 +37,6 @@ app.get("/",(req,res)=>{
     res.send("Hi i am root.");
 });
 
-//ValidationSchema using Middleware
-const validateListing = (req,res,next)=>{
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=> el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
 
 
 const validateReview = (req,res,next)=>{
@@ -57,59 +49,8 @@ const validateReview = (req,res,next)=>{
     }
 }
 
-//Index Route
-app.get("/listings",wrapAsync(async(req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
-
-// New Route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-});
-
-//Show Route (READ)
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-}));
-
-//Create Route
- app.post("/listings",validateListing,wrapAsync(async(req,res)=>{
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Send Valid data for listing!");
-    // }
-    //let {title,description,image,price,country,location} = req.body;
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
- }));
-
- //Edit Route
- app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
- }));
-
- //Update Route
- app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Send Valid data for listing!");
-    // }
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
- }));
-
- //Delete Route
- app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
- }));
+//Single line used for all listings route.
+app.use("/listings",listings);
 
  //Reviews
  //Post Review Route
