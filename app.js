412785d4fa9514cfc6b9e1577f11723edc9d9f5f
Adarsh20021 <adarshkulkarni10@gmail.com>
2026-08-05 16:21:16 +0530

    SignUp User - GET

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingsRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -79,8 +80,9 @@ app.get("/demouser",async(req,res)=>{
 });
 
 //Single line used for all listings and review routes.
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingsRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 
 app.all("/{*any}",(req,res,next)=>{
--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose").default;
 
-console.log(passportLocalMongoose);
-console.log(typeof passportLocalMongoose);
+// console.log(passportLocalMongoose);
+// console.log(typeof passportLocalMongoose);
 
 const userSchema = new Schema({
     email: {
