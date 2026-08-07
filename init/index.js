78fa68be60d0b3cc6c7d51c8bbf5460d8bb03ad1
Adarsh20021 <adarshkulkarni10@gmail.com>
2026-08-07 22:38:04 +0530

    Listing Owner

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj)=>({...obj,owner:"6a72f87ab89f92eb43a4dbc0"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
--- routes/listing.js
+++ routes/listing.js
@@ -31,7 +31,7 @@ router.get("/new",isLoggedIn,(req,res)=>{
 //Show Route (READ)
 router.get("/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing){
         req.flash("error","Listing you requested for does not exist!");
         return res.redirect("/listings");
@@ -49,6 +49,7 @@ router.post("/",isLoggedIn,validateListing,wrapAsync(async(req,res)=>{
         delete req.body.listing.image;
     }
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success","New Listing Created!");
     res.redirect("/listings");
