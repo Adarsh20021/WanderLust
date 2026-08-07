280d73286358974d0254803de06ab99c2d3411d7
Adarsh20021 <adarshkulkarni10@gmail.com>
2026-08-08 00:10:37 +0530

    Authorization for /reviews(v2)

--- routes/review.js
+++ routes/review.js
@@ -4,7 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview,isLoggedIn} = require("../middleware.js");
+const {validateReview,isLoggedIn,isReviewAuthor} = require("../middleware.js");
 
 //Reviews
  //Post Review Route
@@ -22,7 +22,7 @@ const {validateReview,isLoggedIn} = require("../middleware.js");
  }));
 
  //Delete Review Route
- router.delete("/:reviewId", wrapAsync(async(req,res)=>{
+ router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(async(req,res)=>{
     let {id, reviewId} = req.params;
 
     await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
